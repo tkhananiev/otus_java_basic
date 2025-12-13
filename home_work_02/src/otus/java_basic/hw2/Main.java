3901242025119ff863e5fb44dcb2d9342623878d
Timur Khananiev <timur712@gmail.com>
2025-12-13 12:57:44 +0300

    home_work_02.1

---
+++ home_work_02/src/otus/java_basic/hw2/Main.java
@@ -0,0 +1,62 @@
+package otus.java_basic.hw2;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr01 = {3, 7, 9, 8, 5, 1};
+        int[] arr02 = new int[7];
+        printStr("Hello world", 5);
+        printSumArray(arr01);
+        fillArr(arr02, 6);
+       for (int n : arr02) {
+           System.out.print(n);
+       }
+       incrArr(arr02, 2);
+        System.out.println(" ");
+        for (int n : arr02) {
+            System.out.print(n);
+        }
+        System.out.println();
+        sumHalfArr(arr01);
+    }
+
+    static void printStr(String str, int n) {
+       for  (int i = 0; i < n; i++) {
+           System.out.println(str);
+       }
+    }
+    static void printSumArray(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 5) {
+                sum += arr[i];
+            }
+        }
+        System.out.println(sum);
+    }
+    static void fillArr(int[] arr, int n) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = n;
+        }
+    }
+    static void incrArr(int[] arr, int n) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] += n;
+        }
+    }
+    static void sumHalfArr(int[] arr) {
+        int firstHalfSum = 0;
+        int secondHalfSum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (i < arr.length / 2) {
+                firstHalfSum += arr[i];
+            }  else {
+                secondHalfSum += arr[i];
+            }
+        }
+        if (firstHalfSum > secondHalfSum) {
+            System.out.println("Сумма элементов первой из половин массива больше и равна " + firstHalfSum);
+        }else {
+            System.out.println("Сумма элементов второй из половин массива больше и равна " + secondHalfSum);
+        }
+    }
+}
