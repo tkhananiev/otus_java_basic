eadc75673c717cd7da99aa5c0fb251baaa832828
Timur Khananiev <timur712@gmail.com>
2026-01-29 12:40:43 +0300

    ООП: Наследование и полиморфизм

---
+++ home_work_05/src/otus/java/basic/hw5/Main.java
@@ -0,0 +1,19 @@
+package otus.java.basic.hw5;
+public class Main {
+    public static void main(String[] args) {
+        Animal[] animals = {
+                new Cat("Murzik", 10, 50),
+                new Dog("Bobik", 12, 4, 60),
+                new Horse("Buyan", 15, 3, 120)
+        };
+        for (Animal animal : animals) {
+            System.out.println("----- " + animal.getName() + " -----");
+
+            animal.run(20);
+            animal.swim(10);
+
+            animal.info();
+            System.out.println();
+        }
+    }
+}
